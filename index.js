fde124370d943a7c1fd276a56f8d2a929284d178
Travis Webb <me@traviswebb.com>
2015-11-29 19:15:32 -0500

    [init] npm init, created stub trailpack index

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const Trailpack = require('trailpack')
+const lib = require('./lib')
+const _ = require('lodash')
+
+/**
+ * Hapi Trailpack
+ * 
+ * Bind application routes to Hapi.js (from trailpack-router)
+ */
+module.exports = class Hapi extends Trailpack {
+
+}
