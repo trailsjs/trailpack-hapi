e568f616ead62084f2e90598129e81d0b3bb85e5
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2015-12-24 13:15:32 -0500

    Removed deps from trails core module

--- archetype/api/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-exports.controllers = require('./controllers')
-exports.models = require('./models')
-exports.policies = require('./policies')
-exports.services = require('./services')
